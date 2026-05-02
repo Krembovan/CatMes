7a5cddafb1efa74378e20f37b608396ee1011ad3
krembovan <krembovan@example.com>
2026-05-03 01:51:34 +0300

    Fix socket and render port

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import json, os, time
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room
+import os
+from flask import send_from_directory
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'skam_1337_prod'
